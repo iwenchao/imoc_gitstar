a3cbf28359cf3b56c71122aba64ac2f57e82158a
iwenchao <iwenchaos@gmail.com>
2018-10-07 15:49:28 +0800

    增加二级页面

--- js/page/RepoDetailPage.js
+++ js/page/RepoDetailPage.js
@@ -4,45 +4,54 @@
  * Description:
  */
 
+import React, { Component } from 'react'
 
+import { StyleSheet, View, WebView } from 'react-native'
 
-import React, {Component} from 'react';
+import * as Constant from '../common/Constant'
+import ViewUtils from '../util/ViewUtils'
+import NavigationBar from '../widget/NavigationBar'
 
-import {StyleSheet, WebView,View} from 'react-native';
-import NavigationBar from "../widget/NavigationBar";
-
-import * as Constant from  '../common/Constant'
-
-const URL = 'http://www.imooc.com';
+const URL = 'http://www.imooc.com'
 
 export default class RepoDetailPage extends Component {
-    // 构造
-    constructor(props) {
-        super(props);
-        // 初始状态
-        this.state = {
-            url:URL
-        };
+  // 构造
+  constructor (props) {
+    super(props)
+    // 初始状态
+    this.state = {
+      url: URL
     }
+  }
 
+  _onBack () {
+    this.props.navigator.pop()
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <NavigationBar
-                    title={'WebView使用'}
-                    style={{backgroundColor:Constant.STATUS_BAR_COLOR}}
-                />
-                <WebView
-                    source={{uri:this.state.url}}
-                />
-            </View>
-        );
-    }
+  componentDidMount () {
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <NavigationBar
+          title={'WebView使用'}
+          style={{backgroundColor: Constant.STATUS_BAR_COLOR}}
+          statusBar={{backgroundColor: Constant.STATUS_BAR_COLOR}}
+          leftButton={ViewUtils.getLeftButton(() => {
+            this._onBack()
+          })}
+        />
+        <WebView
+          source={{uri: this.props.item.html_url}}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1
-    }
-});
+  container: {
+    flex: 1
+  }
+})
